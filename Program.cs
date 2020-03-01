713fb7477a94ef7772020d43b5c4b41b4c77c9d7
Kelly Duong <kellyduong@usf.edu>
2020-02-29 20:15:41 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        // the method perform calculation
+        static int Calculate(int a, int b, char flag)
+        {
+            // add
+            if (flag == '+')
+                return a + b;
+
+            // subtract
+            if (flag == '-')
+                return a - b;
+
+            // multiply
+            if (flag == '*')
+                return a * b;
+
+            // divide
+            if (flag == '/')
+                return a / b;
+
+            // return 0 as dummy value
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kelly K Duong");
+
+            // call the method to add two numbers
+            Console.WriteLine("The sum of 6 and 3 is " + Calculate(6, 3, '+') + ".");
+
+            // call the method to subtract two numbers
+            Console.WriteLine("The difference of 6 and 3 is " + Calculate(6, 3, '-') + ".");
+
+            // call the method to multiply two numbers
+            Console.WriteLine("The product of 6 and 3 is " + Calculate(6, 3, '*') + ".");
+
+            // call the method to divide two numbers
+            Console.WriteLine("The quotient of 6 and 3 is " + Calculate(6, 3, '/') + ".");
+        }
+    }
+}
